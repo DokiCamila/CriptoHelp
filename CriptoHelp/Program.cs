ae78c540596b572e77b8f901435fb71344d571fc
Camila Doki <camiladoki@camilas-mbp.bol-us.ibmmobiledemo.com>
2017-11-05 18:30:54 -0200

    incio

---
+++ CriptoHelp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CriptoHelp
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
