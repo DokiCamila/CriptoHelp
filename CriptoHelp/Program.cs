d52786bccbecc0111c5a4a23d7a25ceb2aacd5e0
Camila Doki <camiladoki@Camilas-MacBook-Pro.local>
2017-11-09 19:58:04 -0200

    Program.cs

--- CriptoHelp/Program.cs
+++ CriptoHelp/Program.cs
@@ -4,6 +4,12 @@ namespace CriptoHelp
 {
     class MainClass
     {
+        public static void Cifrar(string frase, int chave){
+
+            int[,] chave;
+
+
+        }
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
